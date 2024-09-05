8774545edbcbcb0bdaf3921782dffeb15df4ef35
David Li <li.davidm96@gmail.com>
2024-09-04 23:52:46 -0400

    fix new paths

--- recipe/build-python.sh
+++ recipe/build-python.sh
@@ -11,8 +11,9 @@ echo "==== INSTALL ${PKG_NAME}"
 $PYTHON -m pip install . -vvv --no-deps --no-build-isolation
 
 if [[ "${PKG_NAME}" == "adbc-driver-manager" ]]; then
-    rm "${SP_DIR}/adbc_driver_manager/adbc.h"
-    rm "${SP_DIR}/adbc_driver_manager/adbc_driver_manager.h"
+    rm "${SP_DIR}/adbc_driver_manager/arrow-adbc/adbc.h"
+    rm "${SP_DIR}/adbc_driver_manager/arrow-adbc/adbc_driver_manager.h"
+    rmdir "${SP_DIR}/adbc_driver_manager/arrow-adbc"
     rm "${SP_DIR}/adbc_driver_manager/adbc_driver_manager.cc"
 fi
 
